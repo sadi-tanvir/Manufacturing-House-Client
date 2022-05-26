7d6d889aa4fb117239c1679debdccdca23c1aadb
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-27 02:40:25 +0600

    added firebase login with google

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from "./pages/shared/Footer/Footer"
 import Purchase from './pages/Purchase/Purchase';
 import Login from './pages/Login-User/Login/Login';
 import Register from './pages/Login-User/Register/Register';
+import RequireAuth from './pages/Login-User/RequireAuth/RequireAuth';
+import CheckAuth from "./pages/Login-User/RequireAuth/CheckAuth"
 
 const App = () => {
   return (
@@ -13,9 +15,21 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/purchase/:productId" element={<Purchase />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/purchase/:productId" element={
+          <RequireAuth>
+            <Purchase />
+          </RequireAuth>
+        } />
+        <Route path="/login" element={
+          <CheckAuth>
+            <Login />
+          </CheckAuth>
+        } />
+        <Route path="/register" element={
+          <CheckAuth>
+            <Register />
+          </CheckAuth>
+        } />
       </Routes>
       <Footer />
     </>
--- src/pages/Login-User/SocialLogin/SocialLogin.js
+++ src/pages/Login-User/SocialLogin/SocialLogin.js
@@ -1,14 +1,41 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
-
+import auth from '../../../firebase.init';
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 
 const SocialLogin = () => {
-    
+    const [user, loading, error] = useAuthState(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+
+    // router
+    let navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
+    const handleSigning = async () => {
+        await signInWithGoogle()
+    }
+
+    useEffect(() => {
+        if(user){
+            navigate(from, { replace: true });
+        }
+        
+        // const sendUserToDB = async () => {
+        //     const res = await axios.put(`${apiBaseUrl}/user/${user.email}`, {name:user.displayName,email:user.email})
+        //     console.log(res);
+        //     setAuthToken(res.data.token)
+        //     localStorage.setItem('token', JSON.stringify(res.data.token))
+        // }
+        // sendUserToDB()
+
+    },[user])
     return (
         <>
-            <button className="btn btn-outline btn-info w-full">CONTINUE WITH GOOGLE</button>
+            <button onClick={handleSigning}className="btn btn-outline btn-info w-full">CONTINUE WITH GOOGLE</button>
         </>
     );
 };
