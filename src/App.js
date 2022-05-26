325f6374384f91485c14c4a9874f3de822d4be0f
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-26 23:48:11 +0600

    added Login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home';
 import Navbar from './pages/shared/Navbar/Navbar';
 import Footer from "./pages/shared/Footer/Footer"
 import Purchase from './pages/Purchase/Purchase';
+import Login from './pages/Login-User/Login/Login';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/purchase/:productId" element={<Purchase />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/Login-User/Login/Login.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+
+
+const Login = () => {
+
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <div className="card w-4/12 bg-base-100 shadow-xl py-5">
+                <div className="card-body">
+                    <h1 className="text-cyan-600 font-bold text-3xl text-center mb-5">Login</h1>
+                    <form className="grid grid-cols-1 gap-5" action="">
+                        <input name="email" type="email" placeholder="Type Email" className="input input-bordered w-full" />
+                        <input name="password" type="password" placeholder="Type Password" className="input input-bordered w-full" />
+                        <button type="submit" className="btn btn-accent text-white font-bold bg-gradient-to-r from-cyan-500 to-cyan-600 border-0 rounded-xl w-full">Login</button>
+                    </form>
+                    <div className="flex flex-col w-full border-opacity-50">
+                        <div className="">
+                            <p className=" text-sm text-center">
+                                New to here?
+                                <span className="text-red-400 cursor-pointer ml-1">Create new account</span>
+                            </p>
+                        </div>
+                        <div className="divider">OR</div>
+                        <div className="">
+                            {/* <SocialLogin /> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
