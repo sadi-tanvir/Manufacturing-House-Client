31cf0e383dfa7d23183ee3becb09c73d88a2aaf8
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-26 13:36:17 +0600

    added footer section

---
+++ src/pages/shared/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-slate-200">
+                <div className="footer flex justify-between items-center">
+                    <div>
+                        <span className="footer-title">Services</span>
+                        <Link to="#" className="link link-hover">Branding</Link>
+                        <Link to="#" className="link link-hover">Design</Link>
+                        <Link to="#" className="link link-hover">Marketing</Link>
+                        <Link to="#" className="link link-hover">Advertisement</Link>
+                    </div>
+                    <div>
+                        <span className="footer-title">Company</span>
+                        <Link to="#" className="link link-hover">About us</Link>
+                        <Link to="#" className="link link-hover">Contact</Link>
+                        <Link to="#" className="link link-hover">Jobs</Link>
+                        <Link to="#" className="link link-hover">Press kit</Link>
+                    </div>
+                    <div>
+                        <span className="footer-title">Legal</span>
+                        <Link to="#" className="link link-hover">Terms of use</Link>
+                        <Link to="#" className="link link-hover">Privacy policy</Link>
+                        <Link to="#" className="link link-hover">Cookie policy</Link>
+                    </div>
+                </div>
+
+                <p className="mt-7 text-center">Copyright 2022 All Rights Reserved</p>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
