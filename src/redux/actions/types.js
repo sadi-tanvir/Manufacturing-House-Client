1b5dcf9927fc2988491f2a6d2386718a94476359
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-26 11:23:22 +0600

    initial commit

---
+++ src/redux/actions/types.js
@@ -0,0 +1,1 @@
+export const IS_CHANGE = "IS_CHANGE"
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux"
+import globalReducer from './global'
+
+const rootReducer = combineReducers({
+    globalReducer
+})
+
+
+export default rootReducer;
